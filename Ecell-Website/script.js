81f3139b95730e83da00a78eeb0007bd984d7e0c
Sambit Pradhan <143246577+Sambit2005-lab@users.noreply.github.com>
2024-11-04 14:47:51 +0530

    Update script.js for event slider animation

--- Ecell-Website/script.js
+++ Ecell-Website/script.js
@@ -123,3 +123,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
     teamCols.forEach((col) => observer.observe(col));
 });
+
+
+
+
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
